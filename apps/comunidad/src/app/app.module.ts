b810ab9a9296b78351acc2430e29a786e7240c70
Joey Pedicini <joey.pedicini@gmail.com>
2022-08-28 16:34:25 -0400

    set up PWA

--- apps/comunidad/src/app/app.module.ts
+++ apps/comunidad/src/app/app.module.ts
@@ -47,6 +47,12 @@ import { AddContactComponent } from './add-contact/add-contact.component';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
