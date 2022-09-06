2cc85de29d64adda82ec1933ba53c13dd00de08d
Joey Pedicini <joey.pedicini@gmail.com>
2022-09-06 14:15:21 -0400

    add marketing site

---
+++ apps/marketing/pages/index.tsx
@@ -0,0 +1,133 @@
+export default function Example() {
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault();
+
+    // Get data from the form.
+    const data = {
+      email: event.target.email.value,
+    };
+
+    // Send the data to the server in JSON format.
+    const JSONdata = JSON.stringify(data);
+
+    // API endpoint where we send form data.
+    const endpoint = '/api/form';
+
+    // Form the request for sending data to the server.
+    const options = {
+      // The method is POST because we are sending data.
+      method: 'POST',
+      // Tell the server we're sending JSON.
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // Body of the request is the JSON data we created above.
+      body: JSONdata,
+    };
+
+    // Send the form data to our forms API on Vercel and get a response.
+    const response = await fetch(endpoint, options);
+
+    // Get the response data from server as JSON.
+    // If server returns the name submitted, that means the form works.
+    const result = await response.json();
+    alert(`A login link was sent to: ${result.data}`);
+  };
+  return (
+    <div className="bg-white pb-8 sm:pb-12 lg:pb-12">
+      <div className="overflow-hidden pt-8 sm:pt-12 lg:relative lg:py-48">
+        <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-24 lg:px-8">
+          <div>
+            <div>
+              <img className="h-11 w-auto" src="logo.png" alt="Your Company" />
+            </div>
+            <div className="">
+              <div className="mt-6 sm:max-w-xl">
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+                  A community built around daily journaling
+                </h1>
+                <p className="mt-6 text-xl text-gray-500">
+                  The best way to start journaling is to start journaling.
+                  We&apos;ll help you get started.
+                </p>
+              </div>
+              <form
+                onSubmit={handleSubmit}
+                className="mt-12 sm:flex sm:w-full sm:max-w-lg"
+              >
+                <div className="min-w-0 flex-1">
+                  <label htmlFor="hero-email" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="hero-email"
+                    name="email"
+                    type="email"
+                    className="block w-full rounded-md border border-gray-300 px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
+                    placeholder="Enter your email"
+                  />
+                </div>
+                <div className="mt-4 sm:mt-0 sm:ml-3">
+                  <button
+                    type="submit"
+                    className="block w-full rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:px-10"
+                  >
+                    Get Started
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        <div className="sm:mx-auto sm:max-w-3xl sm:px-6">
+          <div className="py-12 sm:relative sm:mt-12 sm:py-16 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+            <div className="hidden sm:block">
+              <div className="absolute inset-y-0 left-1/2 w-screen rounded-l-3xl bg-gray-50 lg:left-80 lg:right-0 lg:w-full" />
+              <svg
+                className="absolute top-8 right-1/2 -mr-3 lg:left-0 lg:m-0"
+                width={404}
+                height={392}
+                fill="none"
+                viewBox="0 0 404 392"
+              >
+                <defs>
+                  <pattern
+                    id="837c3e70-6c3a-44e6-8854-cc48c737b659"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={392}
+                  fill="url(#837c3e70-6c3a-44e6-8854-cc48c737b659)"
+                />
+              </svg>
+            </div>
+            <div className="relative -mr-40 pl-4 sm:mx-auto sm:max-w-3xl sm:px-0 lg:h-full lg:max-w-none lg:pl-12">
+              <img
+                className="w-full rounded-md shadow-xl ring-1 ring-black ring-opacity-5 lg:h-full lg:w-auto lg:max-w-none"
+                src="comunidaddies.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
